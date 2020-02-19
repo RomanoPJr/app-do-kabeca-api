ee212654adba7e6ae0293464f30767021c81193e
Caio Deambrosio <caio.deambrosio@gmail.com>
2020-02-19 01:07:59 -0300

    Improve

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
+  protocol: 'postgres',
   host: process.env.DATABASE_URL,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: true,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // ssl: true,
   define: {
     timestamps: true,
     underscored: true,
