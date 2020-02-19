80f6c2217c1607324b358285c86c7b4326b7c232
Caio Deambrosio <caio.deambrosio@gmail.com>
2020-02-19 00:00:11 -0300

    Include .env and automatizate db:migrate

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
-  username: 'postgres',
-  password: 'docker',
-  database: 'app_do_kabeca',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
